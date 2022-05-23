7f9b127280f25901a1e221edc9aa5c26e6effdf1
Robert Jakobi <robert.jakobi@stud.tu-darmstadt.de>
2022-05-23 15:58:26 +0200

    Add auto-gen of nessessary files

--- DiscordScheduleAutomatum V.3/src/main/java/de/tu_darmstadt/informatik/robert_jakobi/dsa/Main.java
+++ DiscordScheduleAutomatum V.3/src/main/java/de/tu_darmstadt/informatik/robert_jakobi/dsa/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileAttribute;
 
 import de.tu_darmstadt.informatik.robert_jakobi.dsa.bot.Bot;
 
@@ -26,9 +28,29 @@ public class Main {
      *             reading the bot key occurred
      */
     public static void main(String[] args) throws IOException {
-        Files.createDirectories(Paths.get("rec", "logs"));
-        Files.createDirectories(Paths.get("rec", "polls"));
+        setupFileStructure();
         System.setOut(new PrintStream(Files.createFile(Paths.get("rec", "logs", System.currentTimeMillis() + ".log")).toFile()));
         new Bot(Files.readString(Paths.get("rec", "key")));
+    }
+
+    /**
+     * Creates nessesary file structure for Bot.
+     * 
+     * @throws IOException
+     *             Thrown if issues on the creation of the file structure
+     */
+    private static void setupFileStructure() throws IOException {
+        Files.createDirectories(Paths.get("rec", "logs"));
+        Files.createDirectories(Paths.get("rec", "polls"));
+        var key = Paths.get("rec", "key");
+        if (!Files.exists(key)) {
+            Files.createFile(key);
+            System.out.println("Bot-Key needed! Location: " + key.toString());
+            System.exit(0);
+        }
+        var invite_data = Paths.get("rec", "invite.data");
+        if (!Files.exists(invite_data)) {
+            Files.createFile(invite_data);
+        }
     }
 }
