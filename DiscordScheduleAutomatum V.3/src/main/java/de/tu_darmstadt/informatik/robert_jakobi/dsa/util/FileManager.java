f4795275d217ae891978c99931211c0806b94b40
Robert Jakobi <robert.jakobi@stud.tu-darmstadt.de>
2023-06-29 21:48:45 +0200

    sys change

---
+++ DiscordScheduleAutomatum V.3/src/main/java/de/tu_darmstadt/informatik/robert_jakobi/dsa/util/FileManager.java
@@ -0,0 +1,7 @@
+package de.tu_darmstadt.informatik.robert_jakobi.dsa.util;
+
+public class FileManager {
+    public static String loadFromFile(String file) {
+        return null;
+    }
+}
---
+++ DiscordScheduleAutomatum V.3/src/main/java/de/tu_darmstadt/informatik/robert_jakobi/dsa/util/SystemProperties.java
@@ -0,0 +1,9 @@
+package de.tu_darmstadt.informatik.robert_jakobi.dsa.util;
+
+public interface SystemProperties {
+    String resourcePath = "rec";
+
+    String logPath = "logs";
+    String pollsPath = "polls";
+    String keyPath = "key";
+}
